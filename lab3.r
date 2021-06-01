4facefdaa98f96eb367ecbd77bff8723a14c9b8f
franco <franco.labra@usach.cl>
2021-06-01 03:18:26 -0400

    listo

--- lab3.r
+++ lab3.r
@@ -5,9 +5,21 @@ require('wordcloud')
 
 IMDB <- read.table("./IMDB", header=TRUE, sep=",")
 head(IMDB)
+
+############ aproximar ratings##################
+IMDB$Rating <- round(IMDB$Rating,1)
 attach(IMDB)
 
-################## preprocesamiento
+############# separar conjunto de entrenamiento y test ######################
+smp_size <- floor(0.80 * nrow(IMDB))
+
+set.seed(123)
+train_ind <- sample(seq_len(nrow(IMDB)), size = smp_size)
+
+train <- IMDB[train_ind, ]
+test <- IMDB[-train_ind, ]
+
+################## preprocesamiento ##################################
 
 pre_procesamiento <- function(texto) {
   texto = tm_map(texto,content_transformer(removePunctuation))
@@ -26,21 +38,60 @@ pre_procesamiento <- function(texto) {
 }
 
 
-corpus = Corpus(VectorSource(Text))
+corpusTrain = Corpus(VectorSource(train$Text))
+corpusTest = Corpus(VectorSource(test$Text))
 
-summary(corpus)
 
+corpusTrain = pre_procesamiento(corpusTrain)
+matrixTrain = DocumentTermMatrix(corpusTrain)
+sparseTrain <- as.compressed.matrix(matrixTrain)
 
-corpus = pre_procesamiento(corpus)
-matrix = DocumentTermMatrix(corpus)
-sparse <- as.compressed.matrix(matrix)
-
-f <- tune.maxent(sparse[1:500], Rating[1:500], nfold = 3, showall = TRUE, verbose = TRUE)
+f <- tune.maxent(sparseTrain, train$Rating, nfold = 3, showall = TRUE, verbose = TRUE)
 print(f)
 
+#mejor:
+#11 l1 = 0.0, l2 = 0.8, usesgd = 0, heldout = 0
+
+modelo <- maxent(sparseTrain, train$Rating, l1_regularizer = 0.0, l2_regularizer = 0.8, use_sgd = FALSE, set_heldout = 0, verbose = TRUE)
+
+corpusTest = pre_procesamiento(corpusTest)
+matrixTest = DocumentTermMatrix(corpusTest)
+sparseTest <- as.compressed.matrix(matrixTest)
+
+resultado <- predict(modelo, sparseTest)
+
+recuperadoRelevante = 0
+recuperadoNoRelevante = 0
+noRecuperadoRelevante = 0
+noRecuperadoNoRelevante = 0
+
+for(i in 1:nrow(resultado)){
+  row = resultado[i, ]
+  label = as.numeric(row[1])
+  col <- which(colnames(resultado) == as.character(label))
+  
+  #caso relevante
+  if(label >= 0.7){
+    #Recuperado
+    if(as.numeric(row[col]) >= 0.7){
+      recuperadoRelevante = recuperadoRelevante + 1
+    }
+    #No recuperado
+    else{
+      noRecuperadoRelevante = noRecuperadoRelevante + 1
+    }
+  }
+  #caso no relevante
+  else{
+    #recuperado
+    if(as.numeric(row[col]) >= 0.7){
+      recuperadoNoRelevante = recuperadoNoRelevante + 1
+    }
+    #no recuperado
+    else{
+      noRecuperadoNoRelevante = noRecuperadoNoRelevante + 1
+    }
+  }
+}
 
 
-modelo <- maxent(sparse[1:500], Rating[1:500], l1_regularizer = 0.0, l2_regularizer = 0.2, use_sgd = FALSE, set_heldout = 0, verbose = TRUE)
-
-
-resultado <- predict(modelo, sparse[501:1000])
